34392cbad8024d9f29fcf78be40b12a1cfdd8b1f
rene devleeschauwer <rene.devleeschauwer@ikan.be>
2024-12-18 07:22:09 -0600

    Config test with Kobee tagging

--- cobol/COBDDB2.cbl
+++ cobol/COBDDB2.cbl
@@ -4,7 +4,7 @@
          WORKING-STORAGE SECTION.
 
          01  WS-SQLREAD           PIC X(1).
-      *** IKAN DEMO with DBB Plus *** 
+      *** IKAN DEMO config *** 
       *** INCLUDING SQLCA COPYBOOK ******
            EXEC SQL
                 INCLUDE SQLCA
