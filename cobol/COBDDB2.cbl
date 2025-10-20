0d1a8c4b8ed9671ad2a6a55cde152c3d75812de2
rene devleeschauwer <rene.devleeschauwer@ikan.be>
2025-10-20 03:41:40 -0500

    Test config Demo RcMac

--- cobol/COBDDB2.cbl
+++ cobol/COBDDB2.cbl
@@ -4,7 +4,7 @@
          WORKING-STORAGE SECTION.
 
          01  WS-SQLREAD           PIC X(1).
-      *** IKAN DEMO Configuration Gerard 17/06/2025 Plus *** 
+      *** IKAN DEMO Configuration Gerard 20/10/2025 *** 
       *** INCLUDING SQLCA COPYBOOK ******
            EXEC SQL
                 INCLUDE SQLCA
